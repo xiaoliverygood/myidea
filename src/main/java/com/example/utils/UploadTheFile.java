58c30d54495055b718941d4c95cbcd2694056984
L <123456@qq.com>
2023-04-21 18:32:35 +0800

    对文件上传这个功能进行修改，还是没能成功

--- src/main/java/com/example/utils/UploadTheFile.java
+++ src/main/java/com/example/utils/UploadTheFile.java
@@ -9,10 +9,14 @@ import java.io.IOException;
 
 public class UploadTheFile {
     public static String TeacherUpload(String name, MultipartFile file) throws IOException {
-        String fileName = "L:\\桌面文件\\plan of winter holiday\\MyFirstProject\\First\\File"+name+".txt";
-        FileOutputStream fileObj = new FileOutputStream(fileName);//请求头将文件上传到了file里面，我们用文件流转存
+        //获取文件的原始名称
+       // String fileName = file.getOriginalFilename();
+// 获取文件的扩展名
+        //String extension = org.apache.commons.io.FilenameUtils.getExtension(fileName);
+        String fileName2 = "L:\\桌面文件\\plan of winter holiday\\MyFirstProject\\First\\File"+".png";
+        FileOutputStream fileObj = new FileOutputStream(fileName2);//请求头将文件上传到了file里面，我们用文件流转存
         long fileSize = file.getSize();
-        byte[] buffer = new byte[1024];
+        byte[] buffer = new byte[8192];
         int len;
         while ((len = file.getInputStream().read(buffer)) > 0) {
             fileObj.write(buffer, 0, len);
