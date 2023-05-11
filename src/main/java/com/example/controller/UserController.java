4aeb215fbd95628aa424de6fca65e28e938aea63
L <123456@qq.com>
2023-05-11 16:57:04 +0800

    基本上没bug了

--- src/main/java/com/example/controller/UserController.java
+++ src/main/java/com/example/controller/UserController.java
@@ -5,6 +5,7 @@ import com.example.model.request.UserRegister;
 import com.example.service.UserService;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,16 +28,22 @@ public class UserController {
     public BaseResponse updataPassword(HttpServletRequest httpServletRequest,String email,String newPassword){
         return userService.updataPassword(httpServletRequest, email,newPassword);
     }
-    @GetMapping("/applyActivity")
-    public BaseResponse applyActivity(HttpServletRequest httpServletRequest,int id){
+    @PostMapping("/applyActivity")
+    public BaseResponse applyActivity(HttpServletRequest httpServletRequest,Integer id){
         return userService.applyActivity(httpServletRequest,id);
     }
     @PostMapping("/singinActivity")
     public BaseResponse singinActivity(HttpServletRequest httpServletRequest,String SinginCode,int id){
+        System.out.println("签到");
+        System.out.println(id);
+        System.out.println(SinginCode);
         return userService.singinActivity(httpServletRequest,SinginCode,id);
     }
     @PostMapping("/singoutActivity")
     public BaseResponse singoutActivity(HttpServletRequest httpServletRequest,String SingOutCode,int id){
+        System.out.println("签退");
+        System.out.println(id);
+        System.out.println(SingOutCode);
         return userService.singoutActivity(httpServletRequest,SingOutCode,id);
     }
     @GetMapping("/findMyAllActivity")
